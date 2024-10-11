4faeaeaafb3019664ab2c235e4325b2bab195ffb
com140 <riddhima.kejriwal@daffodilsw.com>
2024-10-11 12:46:49 +0530

    signup ui

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LoginHeaderComponent } from './login-header/login-header.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-    LoginHeaderComponent
   ],
   imports: [
     BrowserModule,
