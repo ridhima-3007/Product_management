cbc00dfa71bb164a92dff95fcc608f2414537c90
Bhawna793 <bhawna.bhatia@daffodilsw.com>
2024-10-11 14:18:38 +0530

    connected signup

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const PORT = 8000;
 const mongoose=require('mongoose');
 const user=require('./models/user');
 const UserRoute=require('./routes/user')
 
 
-
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- FrontEnd/src/app/components/auth/sign-up/sign-up.component.ts
+++ FrontEnd/src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import{UserService} from 'src/app/Services/user.service'
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+ 
+  user = {
+    name: '',
+    email: '',
+    mobile: null,
+    password: ''
+};
 
+constructor(private userService: UserService,private router:Router) {}
+
+onSignup(form: NgForm) {
+    if (form.invalid) {
+        return; 
+    }
+
+    this.userService.signup(this.user).subscribe(
+        response => {
+            console.log('User signed up successfully:', response);
+            this.router.navigate(['/login']);
+        },
+        error => {
+            console.error('Error signing up:', error);
+        }
+    );
+}
 }
