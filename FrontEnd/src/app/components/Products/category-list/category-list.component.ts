50ee4b125fc00f5de637a0ff765249fe6a1dcf75
Bhawna793 <bhawna.bhatia@daffodilsw.com>
2024-10-17 18:55:04 +0530

    Add product validations

--- FrontEnd/src/app/components/Products/category-list/category-list.component.ts
+++ FrontEnd/src/app/components/Products/category-list/category-list.component.ts
@@ -4,26 +4,30 @@ import { CategoryService } from 'src/app/Services/category.service';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
-
-export class CategoryListComponent implements OnInit{
+export class CategoryListComponent implements OnInit {
   categories: any[] = [];
-  subcategories: any[] = []
+  subcategories: any[] = [];
+  imagepaths = [
+    '/assets/images/1.png',
+    '/assets/images/2.png',
+    '/assets/images/3.png',
+    '/assets/images/4.png',
+    '/assets/images/5.png',
+    '/assets/images/6.png',
+  ];
 
-  constructor(private categoryservice:CategoryService){}
+  constructor(private categoryservice: CategoryService) {}
 
   ngOnInit(): void {
-
     this.categoryservice.getCategories().subscribe(
-      (data)=>{
-        this.categories=data;
+      (data) => {
+        this.categories = data;
       },
-      (error)=>{
-        console.log("Error occured",error)
+      (error) => {
+        console.log('Error occured', error);
       }
-    )
+    );
   }
-
-
 }
--- FrontEnd/src/app/components/Products/productupload/productupload.component.ts
+++ FrontEnd/src/app/components/Products/productupload/productupload.component.ts
@@ -2,50 +2,92 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CategoryService } from 'src/app/Services/category.service';
-import { Router } from '@angular/router'
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { Router } from '@angular/router';
+import { ToasterService } from 'src/app/sharedServices/toastr.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-productform',
   templateUrl: './productupload.component.html',
-  styleUrls: ['./productupload.component.css']
+  styleUrls: ['./productupload.component.css'],
 })
-
-export class ProductuploadComponent implements OnInit{
+export class ProductuploadComponent implements OnInit {
   productForm: FormGroup;
   selectedCoverFile: File | null = null;
   selectedFiles: File[] = [];
   categories: any[] = [];
-  subcategories: any[] = []
+  subcategories: any[] = [];
+  allowedTypes: string[] = ['image/jpeg', 'image/png', 'image/gif','image/jpg','image/webp','image/svg'];
+   errorMessage:string|null=null;
+   maxFileSize=5*1024*1024;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private categoryservice: CategoryService, private router: Router,private snackbar:MatSnackBar) {}
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private categoryservice: CategoryService,
+    private router: Router,
+    private toasterservice:ToasterService,
+  ) {}
 
   ngOnInit(): void {
     this.productForm = this.fb.group({
-      name: ['', Validators.required],
-      price: ['', Validators.required, Validators.pattern('^[0-9]+$')],
-      description: ['', Validators.required],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^(?=.*[a-zA-Z])(?![0-9]+)[a-zA-Z0-9 ]{1,20}$')
+        ]
+      ],
+      price: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]\\d{0,9}(\\.\\d{1,3})?$')
+        ]
+      ],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(30)
+        ]
+      ],
       category: ['', Validators.required],
       subcategory: ['', Validators.required],
-      Quantity: ['', Validators.required, Validators.pattern('^[0-9]+$')],
-      discount: ['', Validators.required, Validators.pattern('^[0-9]+$')],
+      Quantity: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]+$')
+        ]
+      ],
+      discount: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[0-9]\\d{0,9}(\\.\\d{1,3})?$')
+        ]
+      ],
       coverImage: [null, Validators.required],
-      images: [null]
+      images: [null],
     });
+    
 
     this.categoryservice.getCategories().subscribe(
       (data) => {
-        this.categories = data
+        this.categories = data;
       },
       (error) => {
-        console.log("Error Fetching Detais", error)
+        console.log('Error Fetching Detais', error);
       }
     );
 
-    this.productForm.get('category').valueChanges.subscribe(selectedCategory => {
-      this.updateSubcategories(selectedCategory);
-    });
+    this.productForm
+      .get('category')
+      .valueChanges.subscribe((selectedCategory) => {
+        this.updateSubcategories(selectedCategory);
+      });
   }
 
   updateSubcategories(selectedCategory: string): void {
@@ -55,7 +97,9 @@ export class ProductuploadComponent implements OnInit{
       return;
     }
 
-    const category = this.categories.find(cat => cat.name === selectedCategory);
+    const category = this.categories.find(
+      (cat) => cat.name === selectedCategory
+    );
 
     if (category) {
       this.productForm.get('subcategory').setValue('');
@@ -63,17 +107,45 @@ export class ProductuploadComponent implements OnInit{
     } else {
       this.subcategories = [];
     }
-
   }
 
-
   onCoverFileSelected(event: any): void {
+    const fileInput = event.target as HTMLInputElement;
+    const files = fileInput.files;
+
+    if (files && files.length > 0) {
+      const file = files[0]; 
+      this.validateFile(file); 
+    }
     this.selectedCoverFile = event.target.files[0];
   }
 
   onFileSelected(event: any): void {
+    const fileInput = event.target as HTMLInputElement;
+    const files = fileInput.files;
+
+    if (files && files.length > 0) {
+      const file = files[0]; 
+      this.validateFile(file); 
+    }
     this.selectedFiles = Array.from(event.target.files);
   }
+
+  validateFile(file: File): void {
+    this.errorMessage = null; 
+
+    if (!this.allowedTypes.includes(file.type)) {
+      this.toasterservice.showError('','Invalid File Type')
+      return;
+    }
+
+    if (file.size > this.maxFileSize) {
+      this.toasterservice.showError('','File Exceeds 5MB')
+      return;
+    }
+
+  }
+  
 
   onSubmit(): void {
     const formData: FormData = new FormData();
@@ -86,28 +158,31 @@ export class ProductuploadComponent implements OnInit{
     formData.append('discount', this.productForm.get('discount')?.value);
 
     if (this.selectedCoverFile) {
-      formData.append('coverImage', this.selectedCoverFile, this.selectedCoverFile.name);
+      formData.append(
+        'coverImage',
+        this.selectedCoverFile,
+        this.selectedCoverFile.name
+      );
     }
 
-    this.selectedFiles.forEach(file => {
+    this.selectedFiles.forEach((file) => {
       formData.append('images', file, file.name);
     });
 
-    this.http.post('http://localhost:8000/api/products', formData, { withCredentials: true }).subscribe(
-      response => {
-        console.log('Product created successfully!', response);
-        this.snackbar.open("Product Created Successfully", "Close", {
-          verticalPosition: 'top',
-        });
-        this.productForm.reset();
-      },
-      error => {
-        console.error('Error creating product', error);
-        this.snackbar.open("Product not Created", "Close", {
-          verticalPosition: 'top',
-        });
-      }
-
-    );
+    this.http
+      .post( environment.APIURL+'/api/products', formData, {
+        withCredentials: true,
+      })
+      .subscribe(
+        (response) => {
+          console.log('Product created successfully!', response);
+         this.toasterservice.showSuccess('','Product Created Successfully')
+          this.router.navigate(['/myListings']);
+        },
+        (error) => {
+          console.error('Error creating product', error);
+          this.toasterservice.showError('','Error Occured')
+        }
+      );
   }
 }
